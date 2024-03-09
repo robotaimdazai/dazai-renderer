c43744292d7f79bd537c2729afba5cf138a8c083
Bilal Azhar <90259416+robotaimdazai@users.noreply.github.com>
2024-03-09 17:51:04 +0100

    Init
    Init with Glad, GLFW

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+﻿// DazaiRenderer.cpp : Defines the entry point for the application.
+//
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+using namespace std;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
+	auto window = glfwCreateWindow(800,600, "Dazai",NULL,NULL);
+	
+	if (window == NULL) {
+		std::cout << "Failed to create window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+
+	glViewport(0,0,800,600);
+	glClearColor(0.0f,0.0f,0.1f,1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	
+	return 0;
+}
